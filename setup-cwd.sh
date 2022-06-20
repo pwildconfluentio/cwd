81175b071bf58dc7c96e318ab2365e1063b42b1e
Phil Wild <83997558+pwildconfluentio@users.noreply.github.com>
2022-06-20 13:23:02 +0800

    updates

--- setup-cwd.sh
+++ setup-cwd.sh
@@ -20,9 +20,9 @@ sudo apt-get install -q -y jq apache2 python3-pip libapache2-mod-wsgi-py3 python
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.2.2-amd64.deb
 sudo dpkg -i filebeat-8.2.2-amd64.deb
 rm filebeat-8.2.2-amd64.deb
-docker-compose up -d
+sudo docker-compose up -d
 SEDCMD="s/===WEBHOSTNAME===/$WEBHOSTNAME/g"
-sed -e '$SEDCMD' cwd/apache2/sites-available/wordle.conf > wordle.conf
+sed -e $SEDCMD cwd/apache2/sites-available/wordle.conf > wordle.conf
 sudo cp wordle.conf /etc/apache2/sites-available/
 sudo cp -r cwd/wsgi /var/www/wsgi
 sudo chown -R www-data:www-data /var/www/wsgi
